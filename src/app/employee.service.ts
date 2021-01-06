ba19aced468c3142599d2b9fabd64fa3630aee9f
Dmitry Babanin <babanin.dima@gmail.com>
2021-01-06 03:37:38 +0300

    feat: Added creating employees

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,11 +8,21 @@ import {Employee} from './employee';
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://localhost:8080/api/v1/employees';
+  private baseUrl = 'http://localhost:8080/api/v1/';
+  private employees = 'employees';
+  private createEmployee = 'employee/create';
 
   constructor(private httpClient: HttpClient) { }
 
+  getUrl(pathToAdd: string): string {
+    return `${this.baseUrl + pathToAdd}`;
+  }
+
   getAll(): Observable<Employee[]>{
-    return this.httpClient.get<Employee[]>(`${this.baseUrl}`);
+    return this.httpClient.get<Employee[]>(this.getUrl(this.employees));
+  }
+
+  create(employee: Employee): Observable<object> {
+    return this.httpClient.post(this.getUrl(this.createEmployee), employee);
   }
 }
